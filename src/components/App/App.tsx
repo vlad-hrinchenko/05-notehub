f30163b15f8fb570ebaa65ee56af9b4205d58faf
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-06-06 18:12:35 +0300

    change

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import {
   useQueryClient,
   keepPreviousData,
 } from "@tanstack/react-query";
-import { fetchNotes, deleteNote } from "../../services/noteServices";
+import { fetchNotes, deleteNote } from "../../services/temp";
 import NoteList from "../NoteList/NoteList";
 import NoteModal from "../NoteModal/NoteModal";
 import SearchBox from "../SearchBox/SearchBox";
